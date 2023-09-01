4fe81a582022a1d8bc6afb3a80c52c6a05fd6e66
gucchunchun <yunataniguchi23@gmail.com>
2023-09-01 12:01:47 +0100

    Add Style Sketch&Box Feature with Adding Box on Page
    *IF the sketch is 'flex' contianer=>box size is 100 devided by number of box in row setting\n*If the sketch is 'grid'=>set sketch's grid template number of box in row setting * number of box in row setting even size & boxes in the sketch width would be 100% of the griid area

---
+++ index.ts
@@ -0,0 +1,28 @@
+const sketches = document.querySelectorAll('.sketch');
+
+const BOX_IN_A_ROW = 5;
+
+function addBoxes(sketch: HTMLElement, width: number = 100) {
+    //add 5*5 boxes
+    for (let i = 0; i < BOX_IN_A_ROW ** 2; i++) {
+        let box = document.createElement('div');
+        box.className = 'sketch__box';
+        box.style.width = `${width}%`;
+        sketch.appendChild(box);
+    }  
+}
+
+window.addEventListener('load', () => {
+    sketches.forEach((elem)=>{
+        const sketch = elem as HTMLElement;
+        if (sketch.classList.contains('sketch--flex')){
+            addBoxes(sketch, 100/BOX_IN_A_ROW);
+        }else if (sketch.classList.contains('sketch--grid')) {
+            addBoxes(sketch);
+            sketch.style.gridTemplate = `repeat(${BOX_IN_A_ROW}, 1fr) / repeat(${BOX_IN_A_ROW}, 1fr)`;
+        }
+        else {
+            throw new Error('found sketch without flex/grid');
+        }
+    })
+});
